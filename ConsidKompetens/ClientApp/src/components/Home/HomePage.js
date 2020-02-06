559e67b107100f333f9efcc7474137e0f280a54e
erlama <anaoleo0@gmail.com>
2020-02-06 13:12:18 +0100

    Created home page

---
+++ ConsidKompetens/ClientApp/src/components/Home/HomePage.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import HomeImage from '../../images/hero-image.jpg';
+import '../../css/Home.css';
+import SearchIcon from '../../images/search.png';
+
+const HomePage = () => {
+    return(
+        <div className="homeContainer">
+            <input className="homeInput" type="text" placeholder="Sök.."/>
+            <button className='search-button'><img src={SearchIcon} alt="Search-icon"/></button>
+            <img className="homeImg" src={HomeImage} alt="ConsidCoffee"/>
+            <div>
+                <label>Välj kontor</label>
+            </div>
+        </div>
+    )
+}
+
+export default HomePage;
