28b525de07566fb3c715fd2538d3dbdd0783f032
Pamp <anders_pamp@hotmail.com>
2020-02-10 14:25:13 +0100

    Add rearrangment of project and register/login functionality

--- ConsidKompetens_Core/Models/BaseEntity.cs
+++ ConsidKompetens_Core/Models/BaseEntity.cs
@@ -11,7 +11,7 @@ namespace ConsidKompetens_Core.Models
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     [Key]
     public int Id { get; set; }
-    public DateTime Created { get; set; }
+    public DateTime Created { get; set; }=DateTime.Now;
     public DateTime Modified { get; set; }
   }
 }
