498e446e8f0302b77e379245d9cb108d7555d124
Pamp <anders_pamp@hotmail.com>
2020-02-10 15:43:32 +0100

    Erase migrations and add new ones for edited models

--- ConsidKompetens_Core/Interfaces/ISearchDataService.cs
+++ ConsidKompetens_Core/Interfaces/ISearchDataService.cs
@@ -9,5 +9,6 @@ namespace ConsidKompetens_Core.Interfaces
     Task<List<OfficeModel>> GetSelectedOfficesAsync(List<int> selectedOffices);
     Task<List<ProfileModel>> GetProfilesByCompetenceAsync(int competenceId);
     Task<List<ProfileModel>> GetProfilesByNameAsync(string input);
+
   }
 }
