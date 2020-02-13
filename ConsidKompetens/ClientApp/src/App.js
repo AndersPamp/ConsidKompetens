274be625d38ff4402a8bdf09fee043fd60b8303b
erlama <anaoleo0@gmail.com>
2020-02-13 20:44:01 +0100

    Started with Register and login funtion

--- ConsidKompetens/ClientApp/src/App.js
+++ ConsidKompetens/ClientApp/src/App.js
@@ -1,18 +1,19 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import HomePage from './components/Home/HomePage';
 import LoginPage from './components/Login/LoginPage';
 import RegisterPage from './components/Register/RegisterPage';
 import UserPage from './components/Users/UserPage';
+import DetailsPage from './components/Details/DetailsPage';
 import NavMenu from './components/Header/NavMenu';
-import {getJwt} from '../src/Helper/jwt';
-import jwt_decode from 'jwt-decode';
+// import {getJwt} from '../src/Helper/jwt';
+// import jwt_decode from 'jwt-decode';
 import { Footer } from '../src/components';
 
 function App() {
 
-  const [ loggedIn, setLoggedIn ] = useState(false);
+  //const [ loggedIn, setLoggedIn ] = useState(false);
   
   //  getUser = () => {
   //       const jwt = getJwt();
@@ -43,9 +44,10 @@ function App() {
       <NavMenu/>
       <Switch>
         <Route exact path="/" component={() => <HomePage />}/>
-        <Route path="/login" component={() => <LoginPage succeeded={this.loginSucceeded} failed={this.loginFailed} loggedIn={loggedIn}/>}/>
-        <Route path="/register" component={() => <RegisterPage/>}/>
-        <Route path="/user" component={() => <UserPage user={this.getUser()}/>}/>
+        <Route path="/login" component={() => <LoginPage/>}/>
+        <Route path="/register" component={RegisterPage}/>
+        <Route path="/user" component={() => <UserPage />}/>
+        <Route path="/details" component={DetailsPage}/>
         {/* <Route exact path='/' loggedIn = {loggedIn} component={HomePage} />
         <Route path='/login' success={this.loginHandle} failed={this.loginFailed} loggedIn = {loggedIn}  component={LoginPage}/>
         <Route path='/register' component={RegisterPage}/>
--- ConsidKompetens/ClientApp/src/components/Register/RegisterPage.js
+++ ConsidKompetens/ClientApp/src/components/Register/RegisterPage.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, {useState} from 'react';
 import TextField from '@material-ui/core/TextField';
 import { ThemeProvider, makeStyles, createMuiTheme} from '@material-ui/core/styles';
 import { red } from '@material-ui/core/colors';
 import Grid from '@material-ui/core/Grid';
 import '../../css/Login.css';
 import LoginImage from '../../images/consid.woman.jpg';
+import axios from 'axios/index';
+import {getJwt} from '../../Helper/jwt';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -22,8 +24,26 @@ const theme = createMuiTheme({
   },
 });
 
+const jwt = getJwt();
+
 const RegisterPage = () => {
     const classes = useStyles();
+
+    const [userName, setUserName] = useState('');
+    const [passWord, setPassWord] = useState('');
+
+    function submitHendler(e) {
+        e.preventDefault();
+        axios.post('https://localhost:44323/api/register', userName, passWord ,{ headers: {'Authorization': `Bearer ${jwt}`} } )
+            .then(response => {
+                alert('User has been registered');
+                setUserName({userName: ''});
+                setPassWord({passWord: ''});
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    };
 
     return(
         <div> 
@@ -33,6 +53,7 @@ const RegisterPage = () => {
                         <img className='login-img' src={LoginImage} alt="Consid woman"/>
                     </Grid>
                     <Grid item xs={7}>
+                      <form onSubmit={submitHendler}>
                         <div className='login-box'>
                             <div className='login-inner-box'>
                                 <label className='login-label'>Registrera</label>
@@ -40,18 +61,21 @@ const RegisterPage = () => {
                                         className={classes.margin} 
                                         style={{display: 'block'}} 
                                         id='mui-theme-provider-standard-input' 
-                                        label='E-post: '>
+                                        label='E-post:'
+                                        onChange={(event, newValue) => setUserName({ userName: newValue})}>
                                 </TextField>
                                 <TextField 
                                         className={classes.margin} 
                                         style={{display: 'block'}} 
                                         type='password' 
                                         id='mui-theme-provider-standard-input' 
-                                        label='Lösenord: '>
+                                        label='Lösenord:'
+                                        onChange={(event, newValue) => setPassWord({ passWord: newValue})}>
                                 </TextField> 
                                 <button className='login-button'>Registrera</button>
                             </div>              
                         </div>
+                        </form>
                     </Grid>
                 </Grid>
             </ThemeProvider>    
