65e3602bfa903b78de9f8aab3bfad12c5c2dd311
Pamp <anders_pamp@hotmail.com>
2020-02-14 13:18:26 +0100

    Another dbcontext git messup

---
+++ ConsidKompetens_Data/Data/DataDbContext.cs
@@ -0,0 +1,19 @@
+﻿using ConsidKompetens_Core.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ConsidKompetens_Web.Data
+{
+  public class DataDbContext:DbContext
+  {
+    public DbSet<CompetenceModel> CompetenceModels { get; set; }
+    public DbSet<ImageModel> ImageModels { get; set; }
+    public DbSet<LinkModel> LinkModels { get; set; }
+    public DbSet<OfficeModel> OfficeModels { get; set; }
+    public DbSet<ProfileModel> ProfileModels { get; set; }
+    public DbSet<ProjectModel> ProjectModels { get; set; }
+    public DataDbContext(DbContextOptions<DataDbContext>options):base(options)
+    {
+      
+    }
+  }
+}
