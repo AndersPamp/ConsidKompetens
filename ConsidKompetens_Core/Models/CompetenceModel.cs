fa5dc1fb575602b24789f5a11a79a7b2f08588a8
Pamp <anders_pamp@hotmail.com>
2020-03-03 20:28:02 +0100

    Add new migrations (for competenceModel)

---
+++ ConsidKompetens_Core/Models/CompetenceModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsidKompetens_Core.Models
+{
+  public class CompetenceModel : BaseEntity
+  {
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public Level Level { get; set; }
+  }
+
+  public enum Level
+  {
+    Good,
+    Intermediate,
+    Moderate
+  }
+}
