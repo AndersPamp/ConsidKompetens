3cbc6db07d8082d4220b631c000fde69f56a0709
Pamp <anders_pamp@hotmail.com>
2020-03-03 12:47:50 +0100

    Remove migrations and db

--- ConsidKompetens_Core/Models/ProjectProfileRole.cs
+++ ConsidKompetens_Core/Models/ProjectProfileRole.cs
@@ -5,13 +5,10 @@ namespace ConsidKompetens_Core.Models
 {
   public class ProjectProfileRole
   {
-    [Key]
     public int ProjectModelId { get; set; }
     public ProjectModel ProjectModel { get; set; }
-    [Key]
     public int ProfileModelId { get; set; }
     public ProfileModel ProfileModel { get; set; }
-    [Key]
     public int RoleModelId { get; set; }
     public RoleModel RoleModel { get; set; }
   }
