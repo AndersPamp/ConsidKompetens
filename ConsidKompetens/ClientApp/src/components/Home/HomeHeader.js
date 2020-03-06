441131ccfa1e53c7861ba236da193f8d80a7cab3
Erla <erlama85@gmail.com>
2020-03-06 15:11:01 +0100

    Change home page if we want to use later

--- ConsidKompetens/ClientApp/src/components/Home/HomeHeader.js
+++ ConsidKompetens/ClientApp/src/components/Home/HomeHeader.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 import HomeImage from '../../images/hero-img6.jpg';
-import SearchIcon from '../../images/search-icon.png';
+//import SearchIcon from '../../images/search-icon.png';
 import {Redirect} from "react-router-dom";
 
 const HomeHeader = () => {
@@ -12,37 +12,15 @@ const HomeHeader = () => {
             localStorage.removeItem('profile');
             setLoggedOut({loggedOut: true});
     }
-
-    
-    // function handleLogoOut(e) {
-    //     e.preventDefault();
-    //     axios.post('https://localhost:44323/api/logout', user)
-    //     .then((response) => {
-
-    //         if(response.data.success === true){
-    //             localStorage.removeItem('secret');
-    //             setLoggedOut({loggedOut: true});
-    //         }
-    //     }) 
-    // }
-
-    // function handleLogoOut(e) {
-    //     e.preventDefault();
-    //     axios.post('https://localhost:44323/api/logout')
-    //     {
-    //         localStorage.removeItem('secret');
-    //         setLoggedOut({loggedOut: true});
-    //     } 
-    // }
     
     return(
         <>
             {loggedOut ? <Redirect to="/login" /> : null} 
             <button className='min-sida-button'><a href="/profile">Min sida</a></button>   
             <button className='logga-ut-button' onClick={handleLogoOut}>Logga ut</button>
-            <input className="homeInput" type="text" placeholder="Sök.."/>
+            {/* <input className="homeInput" type="text" placeholder="Sök.."/>
             <button className='search-button'><img src={SearchIcon} alt="Search-icon"/></button>
-            <label className='home-welcome-label'>Vad letar du efter?</label>
+            <label className='home-welcome-label'>Vad letar du efter?</label> */}
             <img className="homeImg" src={HomeImage} alt="ConsidCoffee"/>
         </>
     )
--- ConsidKompetens/ClientApp/src/components/Home/HomePage.js
+++ ConsidKompetens/ClientApp/src/components/Home/HomePage.js
@@ -1,31 +1,51 @@
 import React from 'react';
 import { Container } from 'reactstrap';
+import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import offices from '../../Helper/Offices.json';
 import HomeHeader from './HomeHeader';
 import '../../css/Home.css';
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
 
+const theme = createMuiTheme({
+  typography: {
+    htmlFontSize: 12,
+    fontFamily: [
+      'Montserrat', 'sans-serif'
+    ]
+  }
+});
 
 const HomePage = () => {
-    
     
     return(
         <>
         <div className="homeContainer">
            <HomeHeader/>
              <Container>
+             <div className='list-container'>
+             <h2>Välj kontor</h2>
                {offices.map(list => {
                    return(
-                       <a className='office-list-a' href="/details" key={list.id}>
-                       <div className='office-list'>
-                           <h3>{list.office}</h3>
-                           <h6>Kullagatan 3</h6>
-                           <h6>25 486 Göteborg</h6>
-                           <h6>Tel: 256 256 265</h6>
-                       </div>
-                       </a>
+                        <ThemeProvider theme={theme}>
+                            <FormGroup style={{display: 'inline'}}>
+                                <FormControlLabel
+                                    control={
+                                    <Checkbox
+                                        value={list.office}
+                                        color="error"
+                                    />
+                                    }
+                                    label={list.office}
+                                />
+                            </FormGroup>
+                        </ThemeProvider>
                    )
                })}
-              
+               <br/>
+               <button>Sök</button>
+               </div>
             </Container>
         </div>
         </>
@@ -33,3 +53,39 @@ const HomePage = () => {
 }
 
 export default HomePage;
+
+// import React from 'react';
+// import { Container } from 'reactstrap';
+// import offices from '../../Helper/Offices.json';
+// import HomeHeader from './HomeHeader';
+// import '../../css/Home.css';
+
+
+// const HomePage = () => {
+    
+    
+//     return(
+//         <>
+//         <div className="homeContainer">
+//            <HomeHeader/>
+//              <Container>
+//                {offices.map(list => {
+//                    return(
+//                        <a className='office-list-a' href="/details" key={list.id}>
+//                        <div className='office-list'>
+//                            <h3>{list.office}</h3>
+//                            <h6>Kullagatan 3</h6>
+//                            <h6>25 486 Göteborg</h6>
+//                            <h6>Tel: 256 256 265</h6>
+//                        </div>
+//                        </a>
+//                    )
+//                })}
+              
+//             </Container>
+//         </div>
+//         </>
+//     )
+// }
+
+// export default HomePage;
