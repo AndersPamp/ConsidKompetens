5a95e46bbae282d3fec8dd00d0641babe8229c13
Erla <erlama85@gmail.com>
2020-03-09 09:06:24 +0100

    Added imagesmodel to contexten

--- ConsidKompetens/ClientApp/src/Context/PofileContext.js
+++ ConsidKompetens/ClientApp/src/Context/PofileContext.js
@@ -11,6 +11,7 @@ const ProfileContextProvider = ({children}) => {
             title: '',
             office: '',
             cv: '',
+            imageModel: '',
             competense: []
         });
         
